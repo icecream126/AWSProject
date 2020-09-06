82a7323359281f5a666a906eede3733781690bb6
Hyomin Kim <kim123hyomin@gmail.com>
2020-09-07 01:04:49 +0900

    4단원_진행사항

--- src/main/java/org/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,13 @@
 package org.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
     //JpaRepository<Entity 클래스, PK타입> 기본적인 CRUD 메소드가 자동으로 생성됨
     //Entity클래스와 기본 Entity Repository는 함께 위치해야 함
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
